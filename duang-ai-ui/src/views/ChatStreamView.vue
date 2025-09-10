d0d1c42b51faa726ca10c20b2b6b6894dafbcfd0
duang <283406834@qq.com>
2025-09-11 02:39:40 +0800

    用户输入不能为空

--- duang-ai-ui/src/views/ChatStreamView.vue
+++ duang-ai-ui/src/views/ChatStreamView.vue
@@ -6,26 +6,43 @@ import {MdEditor} from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
 const form = reactive({
+  rules: {
+    user: [{
+      required: true,
+      trigger: 'blur',
+      validator: (rule, value, callback) => {
+        if (!value) {
+          callback(new Error('用户输入不能为空'))
+        }
+
+        callback()
+      }
+    }]
+  },
   data: {
     system: '',
     user: ''
   }
 })
 
+const formRef = ref()
+
 const result = ref('')
 
 const loading = ref(false)
 
 const chat = () => {
-  const data = form.data
+  const p = formRef.value.validate()
+  p.then(() => {
+    const data = form.data
+    result.value = ''
 
-  result.value = ''
-
-  loading.value = true
-  api.chatStream(data, c => {
-    result.value += c
-  }).finally(() => {
-    loading.value = false
+    loading.value = true
+    api.chatStream(data, c => {
+      result.value += c
+    }).finally(() => {
+      loading.value = false
+    })
   })
 }
 
@@ -41,11 +58,11 @@ const html = computed(() => {
 <template>
   <div class="container">
     <div>
-      <el-form :model="form.data" label-width="auto">
-        <el-form-item label="提示词">
+      <el-form ref="formRef" :model="form.data" :rules="form.rules" label-width="auto">
+        <el-form-item label="提示词" prop="system">
           <md-editor v-model="form.data.system" style="height: 300px"/>
         </el-form-item>
-        <el-form-item label="用户输入">
+        <el-form-item label="用户输入" prop="user">
           <md-editor v-model="form.data.user" style="height: 300px"/>
         </el-form-item>
       </el-form>
